540f43f79b63ae441007364e0f0e55b57e645747
MichaelB387 <30610205+MichaelB387@users.noreply.github.com>
2018-01-31 17:10:38 +1100

    enabled second guess functionality

--- script.js
+++ script.js
@@ -1,11 +1,27 @@
+var correctGuess = false
 var randomNumber = Math.floor(Math.random() * 6) + 1;
 var guess = prompt('I am thinking of a number between 1 and 6. What is it?')
 
 
 if (parseInt(guess) === randomNumber){
-    document.write('<p>You guessed the number!</p>')
+ correctGuess = true
 
+} else if (parseInt(guess) < randomNumber) {
+    var guessMore = prompt('Try again. The number is greater than ' + guess )
+    if (parseInt(guessMore) === randomNumber) {
+      correctGuess = true
+  }
+
+} else if (parseInt(guess) > randomNumber) {
+   var guessLess = prompt('Try again. The number is less than ' + guess )
+   if (parseInt(guessLess) === randomNumber) {
+    correctGuess = true
+  }
+}
+
+if (correctGuess ){
+    document.write('<p>You guessed the number!</p>')
 }
 else {
-  document.write('<p>Sorry. The correct number was ' + randomNumber + ' </p>')
+    document.write('<p>Sorry. The number was ' + randomNumber + '</p>')
 }
